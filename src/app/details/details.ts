1e87e521dc5c28936bd008d833bec9e9146da9d8
Peter Kipping <pkipping@yahoo.com>
2025-08-05 20:13:26 -0400

    added json server

--- src/app/details/details.ts
+++ src/app/details/details.ts
@@ -58,9 +58,12 @@ export class Details {
   });
 
   constructor() {
-    const housingLocationId = Number(this.route.snapshot.params["id"]);
-    this.housingLocation =
-      this.housingService.getHousingLocationById(housingLocationId);
+    const housingLocationId = parseInt(this.route.snapshot.params["id"], 10);
+    this.housingService
+      .getHousingLocationById(housingLocationId)
+      .then((housingLocation) => {
+        this.housingLocation = housingLocation;
+      });
   }
 
   submitApplication() {
